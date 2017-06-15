5dcadd76208d8207cf96398f90fc39cf4f278ab9
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-06-15 10:40:11 +0800

    user can create post in group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
 
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+  has_many :posts
+  belongs_to :user
+  validates :title, presence: true
 end
